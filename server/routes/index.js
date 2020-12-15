534db298832563cb2008c79107ab6dd8d76ddc17
nam <nam98nd16@gmail.com>
2020-12-15 21:29:15 +0700

    update server

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,5 +3,6 @@ var router = express.Router();
 const service = require("../services/index");
 
 router.get("/", service.test);
+router.use("/accounts", require("./accounts"));
 
 module.exports = router;
