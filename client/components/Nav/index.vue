02513fb3f238594dcdd6bbea376ee45d3f14c427
nam <nam98nd16@gmail.com>
2021-04-07 18:52:38 +0700

    update nav bar; init friends view

--- client/components/Nav/index.vue
+++ client/components/Nav/index.vue
@@ -2,44 +2,51 @@
   <div>
     <a-menu v-model="current" mode="horizontal">
       <a-menu-item key="index">
-        <nuxt-link :to="'/'"> <a-icon type="home" />Home </nuxt-link>
+        <nuxt-link :to="'/'"> <i class="fas fa-home mr-2"></i>Home </nuxt-link>
       </a-menu-item>
       <a-menu-item key="test">
-        <nuxt-link :to="'/test'"> <a-icon type="edit" />Training </nuxt-link>
+        <nuxt-link :to="'/test'">
+          <i class="fas fa-running mr-2"></i>Training
+        </nuxt-link>
       </a-menu-item>
       <a-menu-item key="manage-question">
         <nuxt-link :to="'/manage-question'">
-          <a-icon type="question-circle" />Question Management
+          <i class="fas fa-question-circle mr-2"></i>Question Management
         </nuxt-link>
       </a-menu-item>
       <a-menu-item v-if="isAdmin" key="manage-subject">
         <nuxt-link :to="'/manage-subject'">
-          <a-icon type="read" />Subject Management
+          <i class="fas fa-book-open mr-2"></i>Subject Management
         </nuxt-link>
       </a-menu-item>
       <a-menu-item key="performance">
         <nuxt-link :to="'/performance'">
-          <a-icon type="line-chart" />Performance
+          <i class="fas fa-chart-line mr-2"></i>Performance
         </nuxt-link>
       </a-menu-item>
       <a-menu-item key="leaderboard">
         <nuxt-link :to="'/leaderboard'">
-          <a-icon type="crown" />Leaderboard
+          <i class="fas fa-crown mr-2"></i>Leaderboard
+        </nuxt-link>
+      </a-menu-item>
+      <a-menu-item key="friends">
+        <nuxt-link :to="'/friends'">
+          <i class="fas fa-user-friends mr-2"></i>Friends
         </nuxt-link>
       </a-menu-item>
       <a-menu-item v-if="isAdmin" key="manage-user">
         <nuxt-link :to="'/manage-user'">
-          <a-icon type="usergroup-add" />User Management
+          <i class="fas fa-users-cog mr-2"></i>User Management
         </nuxt-link>
       </a-menu-item>
       <a-menu-item key="profile">
         <nuxt-link :to="'/profile'">
-          <a-icon type="user" />
+          <i class="fas fa-user mr-2"></i>
           {{ getProfileName }}
         </nuxt-link>
       </a-menu-item>
       <a-menu-item @click="handleLogout" key="logout">
-        <a-icon type="logout" />Log out
+        <i class="fas fa-sign-out-alt mr-2"></i>Log out
       </a-menu-item>
     </a-menu>
   </div>
---
+++ client/pages/friends.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <page-title title="Friends" />
+  </div>
+</template>
+
+<script>
+import pageTitle from "../components/page-title.vue";
+import { mapState, mapActions } from "vuex";
+export default {
+  components: { pageTitle },
+  data() {
+    return {};
+  }
+};
+</script>
