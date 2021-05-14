5cc8a4a6b80261ee3d54fa292e07a1e4bebd8d28
nam <nam98nd16@gmail.com>
2021-05-14 21:51:33 +0700

    update route authentication

--- client/middleware/auth.js
+++ client/middleware/auth.js
@@ -2,5 +2,15 @@ import jwt_decode from "jwt-decode";
 export default function({ store, redirect }) {
   if (!localStorage.getItem("token")) {
     return redirect("/login");
+  }else {
+    let token = localStorage.getItem("token");
+    try {
+      const data = jwt_decode(token);
+    } catch(error) {
+      localStorage.clear();
+      return redirect("/login");
+    }
+    let isExpiredToken = jwt_decode(token).exp <= new Date().getTime() / 1000;
+    if (isExpiredToken) redirect("/login");
   }
 }
